caa6cb051608c21703a19271b929d283824e31ac
Male Michael <malmike21@outlook.com>
2017-04-18 16:52:52 +0300

    Created class person

---
+++ person.py
@@ -0,0 +1,11 @@
+class Person(object):
+    def __init__(self, name, age, gender):
+        self.name
+        self.age
+        self.gender
+
+    def communicate(self, message):
+        return message
+
+    def move(self, distance):
+        return distance
