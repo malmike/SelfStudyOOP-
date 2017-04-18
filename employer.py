7cad04d569cf0b590aaa83213b582631835c53b3
Male Michael <malmike21@outlook.com>
2017-04-18 16:53:42 +0300

    Created employer that inherits from employee

---
+++ employer.py
@@ -0,0 +1,9 @@
+from employee import  Employee
+
+class Employer(Employee):
+    def __init__():
+        self.subordinates = []
+
+    def hire():
+        new_sub = new Employee('Bruce Bigirwenkya', 23, 'male' 'cleaner')
+        self.subordinates.append(new_sub)
